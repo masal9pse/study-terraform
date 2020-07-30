8c914d98ab17fba0e4058ba541ac5f8c46e50aaf
masal9pse <returnymgstokh@icloud.com>
2020-07-30 20:56:06 +0900

    Create one subnet and Introduced , an indent automatic adjustment tool

--- handson/main.tf
+++ handson/main.tf
@@ -9,3 +9,15 @@ resource "aws_vpc" "main" {
     Name = "handson"
   }
 }
+
+resource "aws_subnet" "public_1a" {
+ vpc_id = "${aws_vpc.main.id}"
+
+ availability_zone = "ap-northeast-1a"
+
+ cidr_block = "10.0.1.0/24"
+
+ tags = {
+  Name = "handson-public-1a"
+ } 
+}

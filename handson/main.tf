8270ef0fb2c44cb5b97a922df8ddc82c2984d833
masal9pse <returnymgstokh@icloud.com>
2020-07-31 23:16:55 +0900

    Create igw and combine with vpc

--- handson/main.tf
+++ handson/main.tf
@@ -79,3 +79,11 @@ resource "aws_subnet" "private_1d" {
     Name = "handson-private-1d"
   }
 }
+
+resource "aws_internet_gateway" "main" {
+ vpc_id = "${aws_vpc.main.id}"
+
+ tags = {
+  Name = "handson"
+ }
+}

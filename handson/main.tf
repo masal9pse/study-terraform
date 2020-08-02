42b0c35a1a0d68a48ef9feda46c95b40649c83d1
masal9pse <returnymgstokh@icloud.com>
2020-08-02 13:12:52 +0900

    Create another nat gateway

--- handson/main.tf
+++ handson/main.tf
@@ -104,3 +104,20 @@ resource "aws_nat_gateway" "nat_1c" {
     Name = "handson-1c"
   }
 }
+
+resource "aws_eip" "nat_1d" {
+  vpc = true
+
+  tags = {
+    Name = "handson-natgw-1d"
+  }
+}
+
+resource "aws_nat_gateway" "nat_1d" {
+  subnet_id     = "${aws_subnet.public_1d.id}"
+  allocation_id = "${aws_eip.nat_1d.id}"
+
+  tags = {
+    Name = "handson-1d"
+  }
+}

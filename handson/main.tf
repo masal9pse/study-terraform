2481d5ae13d3d824182e106c36b03945478cc988
masal9pse <returnymgstokh@icloud.com>
2020-08-02 21:00:22 +0900

    Access ALB's DNS and return "ok"

--- handson/main.tf
+++ handson/main.tf
@@ -285,3 +285,26 @@ resource "aws_lb" "main" {
   security_groups = ["${aws_security_group.alb.id}"]
   subnets         = ["${aws_subnet.public_1a.id}", "${aws_subnet.public_1c.id}", "${aws_subnet.public_1d.id}"]
 }
+
+# Listener
+# https://www.terraform.io/docs/providers/aws/r/lb_listener.html
+resource "aws_lb_listener" "main" {
+  # HTTPでのアクセスを受け付ける
+  port              = "80"
+  protocol          = "HTTP"
+
+  # ALBのarnを指定します。
+  #XXX: arnはAmazon Resource Names の略で、その名の通りリソースを特定するための一意な名前(id)です。
+  load_balancer_arn = "${aws_lb.main.arn}"
+
+  # "ok" という固定レスポンスを設定する
+  default_action {
+    type             = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      status_code  = "200"
+      message_body = "ok"
+    }
+  }
+}

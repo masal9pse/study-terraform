5216dfb01cbdf8fd924d0bb49a7d0084579add0e
masal9pse <returnymgstokh@icloud.com>
2020-08-02 19:17:43 +0900

    Code ALB infrastructure startup

--- handson/main.tf
+++ handson/main.tf
@@ -240,3 +240,48 @@ resource "aws_route_table_association" "private_1d" {
   subnet_id      = "${aws_subnet.private_1d.id}"
   route_table_id = "${aws_route_table.private_1d.id}"
 }
+
+# SecurityGroup
+# https://www.terraform.io/docs/providers/aws/r/security_group.html
+resource "aws_security_group" "alb" {
+  name        = "handson-alb"
+  description = "handson alb"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  # セキュリティグループ内のリソースからインターネットへのアクセスを許可する
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "handson-alb"
+  }
+}
+
+# SecurityGroup Rule
+# https://www.terraform.io/docs/providers/aws/r/security_group.html
+resource "aws_security_group_rule" "alb_http" {
+  security_group_id = "${aws_security_group.alb.id}"
+
+  # セキュリティグループ内のリソースへインターネットからのアクセスを許可する
+  type = "ingress"
+
+  from_port = 80
+  to_port   = 80
+  protocol  = "tcp"
+
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+# ALB
+# https://www.terraform.io/docs/providers/aws/d/lb.html
+resource "aws_lb" "main" {
+  load_balancer_type = "application"
+  name               = "handson"
+
+  security_groups = ["${aws_security_group.alb.id}"]
+  subnets         = ["${aws_subnet.public_1a.id}", "${aws_subnet.public_1c.id}", "${aws_subnet.public_1d.id}"]
+}
